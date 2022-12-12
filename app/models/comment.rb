2b9aaecfd5fc9df6d081aeb07922bc5784344e13
Maikon Douglas <maikon.douglas@vindi.com.br>
2022-12-11 21:50:35 -0300

    implementation viwes for post and comments and add i18n

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 class Comment < ApplicationRecord
+  validates :author, :body, presence: true
+
   belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class Post < ApplicationRecord
+  validates :title, :body, :author, presence: true
+
+  has_many :comments
 end
