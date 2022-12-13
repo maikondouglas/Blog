0b6dab51cb9be0332f5b5ea5b4132b7ab22c856a
Maikon Douglas <maikon.douglas@vindi.com.br>
2022-12-12 22:09:24 -0300

    add gem friendly_id and pg_search_scope

--- Gemfile
+++ Gemfile
@@ -4,9 +4,11 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '3.1.2'
 gem 'bootsnap', require: false
 gem 'cssbundling-rails'
+gem 'friendly_id'
 gem 'jbuilder'
 gem 'jsbundling-rails'
 gem 'pg', '~> 1.1'
+gem 'pg_search'
 gem 'puma', '~> 5.0'
 gem 'rails', '~> 7.0.4'
 gem 'rails-i18n'
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
+  include PgSearch
+  extend FriendlyId
+
   validates :title, :body, :author, presence: true
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
+
+  friendly_id :title, use: :slugged
+
+  pg_search_scope :search,
+                  against: %i[title body author],
+                  associated_against: { comments: %i[body] }
 end
